b413eb6c79206947423feda96656eed106b8ee5c
Ernesto <eacevedo@debian.cl>
2015-11-15 00:03:06 -0300

    Improved init script

--- init.lua
+++ init.lua
@@ -1,11 +1,33 @@
+wifi.setmode(wifi.STATION)
 if wifi.sta.getip() == nil then
-  print("Connecting to AP...\n")
+    print("Connecting to AP...\n")
+    if file.open("password.txt",r) then
+        local username=file.readline();
+        local password=file.readline();
+        file.close();
+        username=string.sub(username,1,#username-1);
+        password=string.sub(password,1,#password-1);
+        wifi.sta.config(username,password);
+    else
+        print("[Cannot open password.txt]");
+    end
 else
   ip, nm, gw=wifi.sta.getip()
   print("IP Address: ",ip)
   print("Netmask: ",nm)
   print("Gateway Addr: ",gw,'\n')
 end
+
+
+tmr.alarm(1, 1000, 1, function()
+    if wifi.sta.getip() == nil then
+        print("[IP unavaiable, waiting.]")
+    else
+        tmr.stop(1)
+        print("[Connected, IP is "..wifi.sta.getip().."]");
+    end
+end)
+
 
 responseHeader = function(code, type)
     return "HTTP/1.1 " .. code .. "\r\nConnection: close\r\nServer: nunu-Luaweb\r\nContent-Type: " .. type .. "\r\n\r\n"
